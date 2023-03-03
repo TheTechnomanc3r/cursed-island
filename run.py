e2d27bdef4d490cdc14c932b0f9e8fde5336fc14
Robin A <robinand@live.com>
2023-03-03 18:01:45 +0000

    add restart option code

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 
-# Import t
+# Import 
 
 import random
 import os
 from time import sleep
 
 # ASCI color codes
-
 
 #Main logo in ASCI Code
 
@@ -33,6 +32,26 @@ def gameOver():
 ·▀▀▀▀  ▀  ▀ ▀▀  █▪▀▀▀ ▀▀▀      ▀█▄▀▪. ▀   ▀▀▀ .▀  ▀
   """)
 
+def tryAgain():
+    print("You died..")
+    print("Do you wanna try again? y/n")
+
+    tryChoice = input(">> ")
+
+    if tryChoice == ("y"):
+              
+        newGame()
+
+    elif tryChoice == ("n"):
+
+
+        print("Closing game ...")
+        sleep(1)
+        exit()
+                   
+
+
+
 #Complete game logo
 
 def winScreen():
@@ -43,44 +62,114 @@ def winScreen():
 # Function to clear the screen
 
 def clearScreen():
-    input("Press any key to Continue...")
+    input("Press ENTER key to Continue...")
     os.system("clear")
 
+# quick clear
 
-mainLogo()
+def qclear():
+    os.system("clear") 
+
+# Print Prologue
+
+def prologue():
+
+    print("\033[1;32m[Prologue]\n")
+    print("\033[1;30mYour lovely sailbot semester in the caribbean didn´t go as planned,")
+    print("and after going trough a heavy storm you crashed on a deserted island")
+
+    print("The Island is tropical with palms, white sand and a dense jungle is seen in the distant.\n")       
+
+# Starts a new game
+
+def newGame():
+
+    mainLogo()  
+
+    print("\033[1;30mWelcome! Do you want to start a new game? y / n")
+
+    startGame = input(">> ")
+
+    if startGame == ("y"):
+    
+        print("Game Starting...")  
+        sleep(0.3)
+        qclear()
+        prologue()
+        firstChoice()
+
+    elif startGame == ("n"):
+
+        print("Closing game ...")
+        sleep(1)
+        exit()
+
+    else:
+        print("Invalid choice! Please enter y or n")
+        qclear()
+        sleep(0.3)
+        newGame()             
 
 
-print("Welcome! Do you want to start a new game? y / n")
+def firstChoice():
 
-startGame = input(">> ")
+    print("Do you want to go to the jungle or go back to the boat?\n")
+    print("go to \033[1;32m(J)ungle\n")
+    print("\033[1;30mgo back to \033[1;34m(B)oat\n")
 
-if startGame == ("y"):
+    startChoice = input("\033[30m>> ")
+
+    if startChoice == ("j"):
+        print("You head for the jungle!")
+        clearScreen()
+        jungle()
+
+    elif startChoice == ("b"):
+        print("You go back to the boat")
+        sleep(0.3)
+        gameOver()
+        tryAgain()
+
+    else:
+        print("Invalid choice Select J or B")
+        sleep(0.3)
+        firstChoice()
+        qclear()
+
+# Jungle
+def jungle():
+
+    print("You walk trough the dense jungle and suddenly you stumble")
+    print("upon a large river with a hanging bridge on top of it")
+    print("--------------------------------------------------")
+    print("It looks unstable, But your best bet is to continue...\n")
+    print("Do you wanna (R)un fast over or \n walk (S)lowly over it")  
+
+    print("""\033[36m
+    / |           /|    
+   /  |          / |   
+  /   |_________/__|
+ /    /________/___/
+/ |  _________/_|_
+  | /________/__|/
+   _________/___
+  /________/___/
+ _________/____
+/________/___/
+    
+    
+    """) 
+
     
 
-    print("Game Starting...")
-    sleep(0.5)
-    os.system("clear")
 
-elif startGame == ("n"):
-    print("Closing game ...")
-    sleep(1)
-    exit()
+#---------------------- NEW GAME STARTS HERE ------------------------
 
-else:
-    print("Invalid choice! Please enter y or n")             
-
-# Prologue text
-
-print("[Prologue]\n")
-print("\033[1;30mYour lovely sailbot semester in the caribbean didn´t go as planned,")
-print("and after going trough a heavy storm you crashed on this deserted island")
-
-print("The Island is tropical with palms, white sand and a dense jungle is seen in the distant.\n")
+newGame()
 
 
 
-# Starting Choice
+# Bridge
 
-print("Do you want to go to the jungle or go back to the boat?\n")
-print("go to \033[1;32m(J)ungle\n")
-print("\033[1;30mgo back to \033[1;34m(B)oat\n")
+
+# Cave
