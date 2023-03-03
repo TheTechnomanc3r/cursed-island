5487815198ddca2eefc9549ae9ed73e8f43c30e7
Robin A <robinand@live.com>
2023-03-03 10:25:57 +0000

    Add prologue text

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 
+# Import t
+
+import random
+import os
+from time import sleep
+
+
 #Main logo in ASCI Code
 
-print("""\033[32m 
+def mainLogo():
+
+    print("""\033[32m 
    ______                         __   ____     __                __
   / ____/_  _______________  ____/ /  /  _/____/ /___ _____  ____/ /
  / /   / / / / ___/ ___/ _ \/ __  /   / // ___/ / __ `/ __ \/ __  / 
@@ -14,7 +23,7 @@ print("""\033[32m
 #Game over screen in ASCI Code
 
 def gameOver():
-  print("""\033[31m
+    print("""\033[31m
    ▄▄ •  ▄▄▄· • ▌ ▄ ·. ▄▄▄ .           ▌ ▐·▄▄▄ .▄▄▄  
 ▐█ ▀ ▪▐█ ▀█ ·██ ▐███▪▀▄.▀·    ▪     ▪█·█▌▀▄.▀·▀▄ █·
 ▄█ ▀█▄▄█▀▀█ ▐█ ▌▐▌▐█·▐▀▀▪▄     ▄█▀▄ ▐█▐█•▐▀▀▪▄▐▀▀▄ 
@@ -22,11 +31,49 @@ def gameOver():
 ·▀▀▀▀  ▀  ▀ ▀▀  █▪▀▀▀ ▀▀▀      ▀█▄▀▪. ▀   ▀▀▀ .▀  ▀
   """)
 
+#Complete game logo
+
+def winScreen():
+    print("################################")
+    print("### congratulations you won! ###")
+    print("################################")  
+
+# Function to clear the screen
+
+def clearScreen():
+    input("Press any key to Continue")
+    os.system("clear")
 
 
-print("Welcome! Do you want to start a new game? Y/N")
+mainLogo()
 
 
-print("\033[1;30mYour lovely sailbot semester in the caribbean ended up with a crash on a mystic island")
-print("The island is tropical with palms and white sand and a jungle in the distant\n")
+print("Welcome! Do you want to start a new game? y / n")
+
+startChoice = input(">> ")
+
+if startChoice == ("y"):
+    
+
+    print("Game Starting...")
+    sleep(0.5)
+    clearScreen()
+
+elif startChoice == ("n"):
+    print("Closing game ...")
+    sleep(1)
+    exit()
+
+else:
+    print("Invalid choice! Please enter y or n")             
+
+
+
+print("[Prologue]\n")
+print("\033[1;30mYour lovely sailbot semester in the caribbean didn´t go as planned,")
+print("and after going trough a heavy storm you crashed on this deserted island")
+
+print("The Island is tropical with palms and white sand and a dense jungle in the distant\n")
+
+clearScreen()
 
